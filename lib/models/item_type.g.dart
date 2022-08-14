ce8337d86ce3007af113352fadc200531a853934
Hannes Kinnunen <hankilapio@gmail.com>
2022-08-14 12:42:09 +0300

    Fix quotes

--- lib/models/item_type.g.dart
+++ lib/models/item_type.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'item_type.dart';
+part of "item_type.dart";
 
 // **************************************************************************
 // TypeAdapterGenerator
